775c1a4ffadaa7a3e886f61f66c75fb3807465c6
zjhmale <zjhmale@gmail.com>
2016-09-14 21:38:59 +0800

    wrap up test

--- test/xml.test.js
+++ test/xml.test.js
@@ -1,109 +1,69 @@
-let assert    = require("assert")
-let formatter = require('../src/wire/formatter')
-let parser    = require("../src/wire/parser")
-let runP      = require('bennu').parse.run
+let assert = require("assert")
+let xml    = require('../src/xml')
 
-let test1 = "(:protocol-version 1 0)"
-let list1 = [':protocol-version', 1, 0]
+let xml1 = '<feedback object="state" route="0"><state_id val="11"/><feedback_content val="errormsg"><loc start="8" stop="16"/><string>Error:&nbsp;plus_0_r&nbsp;already&nbsp;exists.</string></feedback_content></feedback><value val="fail" loc_s="8" loc_e="16"><state_id val="10"/>Error:&nbsp;plus_0_r&nbsp;already&nbsp;exists.</value>'
+let json1 = 
+{ coqtoproot: {
+  feedback: {
+    object: 'state',
+    route: '0',
+    state_id: { val: '11' },
+    feedback_content: {
+      val: 'errormsg',
+      loc: { start: '8', stop: '16' },
+      string: 'Error: plus_0_r already exists.' }
+  },
+  value: {
+    val: 'fail',
+    loc_s: '8',
+    loc_e: '16',
+    state_id: { val: '10' }
+  }
+}}
+let xml2 = '<feedback object="state" route="0"><state_id val="2"/><feedback_content val="processingin"><string>master</string></feedback_content></feedback><feedback object="state" route="0"><state_id val="1"/><feedback_content val="processed"/></feedback><feedback object="state" route="0"><state_id val="2"/><feedback_content val="processed"/></feedback><value val="good"><option val="some"><goals><list><goal><string>2</string><list/><string>forall&nbsp;n&nbsp;:&nbsp;nat,&nbsp;n&nbsp;+&nbsp;0&nbsp;=&nbsp;n</string></goal></list><list/><list/><list/></goals></option></value>'
+let json2 =
+{ coqtoproot: {
+  feedback: [
+    { object: 'state',
+      route: '0',
+      state_id: { val: '2' },
+      feedback_content: { val: 'processingin', string: 'master' } },
+    { object: 'state',
+      route: '0',
+      state_id: { val: '1' },
+      feedback_content: { val: 'processed' } },
+    { object: 'state',
+      route: '0', 
+      state_id: { val: '2' },
+      feedback_content: { val: 'processed' } }
+  ],
+  value: {
+    val: 'good',
+    option: { val: 'some', goals: { list: [ { goal: { string: [ '2', 'forall n : nat, n + 0 = n' ], list: {} } }, {}, {}, {} ] } }
+  }
+}} 
+let xml3 = '<feedback object="state" route="0"><state_id val="2"/><feedback_content val="processed"/></feedback><value val="good"><option val="some"><goals><list><goal><string>2</string><list/><string>forall&nbsp;n&nbsp;:&nbsp;nat,&nbsp;n&nbsp;+&nbsp;0&nbsp;=&nbsp;n</string></goal></list><list/><list/><list/></goals></option></value>'
+let json3 = 
+{ coqtoproot: { 
+  feedback: {
+    object: 'state',
+    route: '0',
+    state_id: { val: '2' },
+    feedback_content: { val: 'processed' } },
+  value: {
+    val: 'good',
+    option: { val: 'some', goals: { list: [ { goal: { string: [ '2', 'forall n : nat, n + 0 = n' ], list: {} } }, {}, {}, {} ] } }
+  }
+}}
 
-let test2 = "(:set-prompt \"*C:\\Programming\\Idris\\Start\\hello\" 1)"
-let list2 = [":set-prompt", "*C:\\Programming\\Idris\\Start\\hello", 1]
+let objectEqual = (a, b) => {
+  return JSON.stringify(a) == JSON.stringify(b)
+}
 
-let test3 = "(:return (:ok ()) 5)"
-let list3 = [":return", [":ok", []], 5]
-
-let test4 = "(:return (:ok \"Main.a : Nat\" ((0 6 ((:name \"Main.a\") (:implicit :False) (:decor :function) (:doc-overview \"\") (:type \"Nat\"))) (9 3 ((:name \"Prelude.Nat.Nat\") (:implicit :False) (:decor :type) (:doc-overview \"Unary natural numbers\") (:type \"Type\"))) (9 3 ((:tt-term \"AAAAAAAAAAAAAwAAAAAACAAAAQyZWx1ZGU=\"))))) 2)"
-let list4 = [":return", [":ok", "Main.a : Nat", [[0, 6, [[":name", "Main.a"], [":implicit", false], [":decor", ":function"], [":doc-overview", ""], [":type", "Nat"]]], [9, 3, [[":name", "Prelude.Nat.Nat"], [":implicit", false], [":decor", ":type"], [":doc-overview", "Unary natural numbers"], [":type", "Type"]]], [9, 3, [[":tt-term", "AAAAAAAAAAAAAwAAAAAACAAAAQyZWx1ZGU="]]]]], 2]
-
-
-let test5 = "(:return (:ok \"\\\"Z\\\" : String\" ((0 3 ((:name \"\\\"Z\\\"\"))))) 5)"
-let list5 =
-  [
-    ":return",
-    [
-      ":ok",
-      '"Z" : String',
-      [
-        [
-          0,
-          3,
-          [
-            [
-              ":name",
-              '"Z"'
-            ]
-          ]
-        ]
-      ]
-    ],
-    5
-  ]
-
-let test6 = "(:return (:ok \"\\\\__pi_arg => \\\\__pi_arg1 => (__pi_arg1)\") 6)"
-let list6 =
-  [
-    ":return",
-    [
-      ":ok",
-      "\\__pi_arg => \\__pi_arg1 => (__pi_arg1)"
-    ],
-    6
-  ]
-
-let test7 = "(:interpret \":cd C:/Path/to/dir\")"
-let list7 =
-  [
-    ":interpret",
-    ":cd C:/Path/to/dir"
-  ]
-
-suite("The sub-parser(s)",() => {
-  test("for :True and :False should work.", () => {
-    assert.equal(runP(parser.trueP, ':True'), true)
-    assert.equal(runP(parser.falseP, ':False'), false)
-  })
-
-  test("for integers should work.", () => {
-    assert.equal(runP(parser.integerP, '2345'), 2345)
-    assert.equal(runP(parser.integerP, '1'), 1)
-  })
-
-  test("for symbols should work.", () => {
-    assert.equal(runP(parser.symbolP, ':sym'), ':sym')
-  })
-
-  test("for string chars should work.", () => {
-    assert.equal(runP(parser.stringCharP, 'h'), 'h')
-    assert.equal(runP(parser.stringCharP, '\\"'), '"')
-  })
-
-  test("for strings should work.", () => {
-    assert.equal(runP(parser.stringP, '"hello"'), 'hello')
-    assert.equal(runP(parser.stringP, '"\\"Z\\""'), '"Z"')
-    assert.equal(runP(parser.stringP, '"\\"Z\\" : String"'), '"Z" : String')
-  })
-})
-
-suite("A parser", () => {
-  test("should parse to the right list.", () => {
-    assert.deepEqual(parser.parse(test1), list1)
-    assert.deepEqual(parser.parse(test2), list2)
-    assert.deepEqual(parser.parse(test3), list3)
-    assert.deepEqual(parser.parse(test4), list4)
-    assert.deepEqual(parser.parse(test5), list5)
-    assert.deepEqual(parser.parse(test6), list6)
-  })
-
-  test("should serialize back again.", () => {
-    assert.equal(formatter.formatSexp(list1), test1)
-    assert.equal(formatter.formatSexp(list2), test2)
-    assert.equal(formatter.formatSexp(list3), test3)
-    assert.equal(formatter.formatSexp(list4), test4)
-    assert.equal(formatter.formatSexp(list7), test7)
-  })
-
-  test("should serialize common commands.", () => {
-    loadFile = [[':load-file', "idris.idr"], 1]
-    assert.equal(formatter.formatSexp(loadFile), '((:load-file "idris.idr") 1)')
+suite("parse xml", () => {
+  test("parse coqtop result", () => {
+    assert.equal(objectEqual(xml.parse(xml1), json1), true)
+    assert.equal(objectEqual(xml.parse(xml2), json2), true)
+    assert.equal(objectEqual(xml.parse(xml3), json3), true)
   })
 })
